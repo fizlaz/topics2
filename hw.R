b31f2b7638915bee248e8174b42379e63eaea521
Dom <fizlaz@gmail.com>
2016-05-20 18:40:15 +0200

    123

--- hw.R
+++ hw.R
@@ -3,7 +3,7 @@ setwd("~/bgse/15D013_Topics_2/Prof. A. Arratia")
 library(quantmod); library(xts)
 library(fBasics)
 
-getSymbols("AAPL",from="1997-01-01")
+#getSymbols("AAPL",from="1997-01-01")
 sym <- "GOOGL"
 
 # Q1
@@ -21,6 +21,8 @@ acplot <- function(sym){
     df[i,"rtrn"] <- abs(df[i,column]/df[(i-1),column]-1)
   }
   
+  #teffectPlot(df$rtrn)
+  
   df$rtrn2 <- df$rtrn^2
   df$rtrn3 <- df$rtrn^3
   df$rtrn4 <- df$rtrn^4
@@ -33,7 +35,7 @@ acplot <- function(sym){
   ac4 <- acf(df$rtrn4)
   ac5 <- acf(df$rtrn5)
   
-  plot(ac,type="l")
+  plot(ac$acf,type="l")
   lines(ac2$acf, col="red")
   lines(ac3$acf, col="blue")
   lines(ac4$acf, col="yellow")
@@ -42,7 +44,7 @@ acplot <- function(sym){
   
 }
 
-acplot("AAPL")
+acplot("GOOGL")
 
 teffectPlot(df$rtrn)
 
@@ -123,18 +125,204 @@ sp <- read.csv("data/data/SP500_shiller.csv")
 
 divpr <- sp$Real.Dividend/sp$Real.Price
 
+
 splogrtrn <- log_return(sp$SP500)
 
 dta <- data.frame(log_return(sp$SP500),log_return(divpr))
+dta1 <- data.frame(log_return(sp$SP500),log_return(sp$P.E10))
+dta2 <- data.frame(log_return(sp$SP500),log_return(divpr),log_return(sp$P.E10))
+
+dta1 <- dta1[complete.cases(dta1),]
+dta2 <- dta2[complete.cases(dta2),]
+
+set <- data.frame(c(1:10),c(11:20))
 
 lagdata <- function(set,lag){
   x <- data.frame(set[,1])
   for(i in 1:lag){
-    y <- set[-c(nrow(set):(nrow(set)-i+1)),]
+    y <- set[-c(nrow(set):(nrow(set)-i+1)),-1]
     x <- data.frame(x[-1,],y)
   }
   return(x)
 }
 
-x <- lagdata(dta,2)
-head(x)
+lagdata(set,3)
+
+training <- lagdata(dta,5)
+head(training)
+names <- names(training)
+names[1] <- "target"
+names(training) <- names
+
+training1 <- lagdata(dta1,5)
+head(training1)
+names <- names(training1)
+names[1] <- "target"
+names(training1) <- names
+
+training2 <- lagdata(dta2,5)
+head(training2)
+names <- names(training2)
+names[1] <- "target"
+names(training2) <- names
+
+########Nonlinear models#############################
+####### SVM and Neural networks ############
+library(e1071) ##for svm
+library(nnet)
+library(kernlab)
+library(quantmod)
+library(caret) ##for some data handling functions
+library(Metrics)##Measures of prediction error:mse, mae
+library(xts)
+
+##Train model
+##############################################
+##OPTION LAZY: one svm, one nnet built w/o tuning  (or tune by hand)
+#type="C" ##classification
+type="eps-regression" ##regression
+#parameters that can be tuned
+u= -2 ## -3,-2,-1,0,1,2,3
+gam=10^{u}; 
+w= 4.5 ##1.5,-1,0.5,2,3,4
+cost=10^{w}
+
+gam <- 0.01
+cost <- 0.11
+##The higher the cost produce less support vectors, increases accuracy
+##However we may overfit
+svmFit = svm (training[,-1], training[,1],
+              #type=type, 
+              kernel= "radial",
+              gamma=gam,
+              cost=cost
+)
+summary(svmFit)
+##build predictor
+predsvm = predict(svmFit, testing[,-ncol(testing)])
+
+#########TUNING
+
+trainset <- data.frame(training)
+trainset1 <- data.frame(training1)
+trainset2 <- data.frame(training2)
+
+
+# set up the cross-validated hyper-parameter search
+svm_grid_1 = expand.grid(
+  cost = 10^c(-1,0.5,1.5,2,3,4),
+  gamma = 10^c(-3,-2,-1,0,1,2,3)
+)
+
+svm_grid_2 = expand.grid(
+  cost = 0.11,
+  gamma = 0.01
+)
+
+# pack the training control parameters
+svm_trcontrol_1 = trainControl(
+  method = "cv",
+  number = 5,
+  verboseIter = TRUE,
+  returnData = FALSE,
+  returnResamp = "all",               # save losses across all models
+  #classProbs = TRUE,                  # set to TRUE for AUC to be computed
+  #summaryFunction = twoClassSummary,
+  summaryFunction = defaultSummary,
+  allowParallel = TRUE
+)
+
+svm_train_0 = train(
+  x = trainset[,-1],
+  y = trainset[,1],
+  trControl = svm_trcontrol_1,
+  tuneGrid = svm_grid_2,
+  method = "svmLinear2"
+  #kernel = "radial", #radial is default
+  #type="eps-regression"
+)
+
+svm_train_1 = train(
+  x = trainset1[,-1],
+  y = trainset1[,1],
+  trControl = svm_trcontrol_1,
+  tuneGrid = svm_grid_2,
+  method = "svmLinear2"
+  #kernel = "radial", #radial is default
+  #type="eps-regression"
+)
+
+svm_train_2 = train(
+  x = trainset2[,-1],
+  y = trainset2[,1],
+  trControl = svm_trcontrol_1,
+  tuneGrid = svm_grid_2,
+  method = "svmLinear2"
+  #kernel = "radial", #radial is default
+  #type="eps-regression"
+)
+
+
+svm_train_0
+svm_train_1
+svm_train_2
+
+#########
+
+##A nnet with size hidden layers +skip layer. Max iteration 10^4,
+size=6
+nnetFit = nnet(training[,-1], training[,1],
+               size=size,skip=T, maxit=10^4,decay=10^{-2},trace=F,linout=T)
+summary(nnetFit) ##gives description w/weights
+##build predictor type="raw"
+prednet<-predict(nnetFit,testing[,-ncol(testing)],type="raw")
+
+################end of Option Lazy ##############################
+
+
+cv.nn <- function(train, size=10, rounds=250){
+
+  library(nnet)
+
+  c <- which(colnames(train)=="target")
+
+    library(caret)
+    set.seed(1234)
+    flds <- createFolds(train$target,k=5)
+    nnpred <- rep(NA,5)
+
+    for(j in 1:5){
+      ktest <- train[flds[[j]],]
+      ktrain <- train[-flds[[j]],]
+
+      print("fold")
+      print(j) # tracing progress
+      print(nnpred)
+
+      size=6
+      #nnetFit = nnet(ktrain[,-1], ktrain[,1],
+      #         size=size,skip=T, maxit=10^4,decay=10^{-2},trace=F,linout=T)
+
+      nnetFit = nnet(ktrain[,-1], ktrain[,1],
+               size=size, linout=T)
+
+      prednet<-predict(nnetFit,ktest[,-1],type="raw")
+
+
+      print(mse(ktest[,1],prednet))
+
+      nnpred[j] <- mse(ktest[,1],prednet)
+
+    } # end of looping cross validations
+    avg <- mean(nnpred)
+
+    print(nnpred)
+    
+    return(list(vec=nnpred,avg=avg))
+
+} # end of cv.nn
+
+
+cvnn <- cv.nn(training)
+
+# Q4
